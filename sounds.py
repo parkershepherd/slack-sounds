19014adbc28605dfb93a43b96b478f33dded27e0
Casey Fulton <casey.fulton@adelaide.edu.au>
2015-06-26 12:27:45 +0930

    Refactored commands out into a function, added message to bot channel

--- sounds.py
+++ sounds.py
@@ -7,6 +7,7 @@ text2voice = 'espeak'
 sounds_dir = 'sounds'
 filetype = 'mp3'
 debug = True
+bots_channel = 'wcms-bots'
 
 play_regex = re.compile("^play\s([a-z]+)$")
 speak_regex = re.compile("^speak\s([a-zA-Z0-9,'!?\- ]+)$")
@@ -18,6 +19,17 @@ add_sound_regex = re.compile("^add-sound\s([a-z]+)\s<?(https?:\/\/[a-z./]*\?v=[a
 # lol that above regex matches the pattern:
 #     add-sound <token> <yt video url> <start> <duration>
 # start and duration are optional
+
+def action(command, message):
+    global debug
+    global sc
+    global bots_channel
+
+    print message
+    print bots_channel
+    sc.rtm_send_message(bots_channel, message)
+    if debug: print 'Running command: ' + command
+    os.system(command)
 
 whitelist = {}
 with open('whitelist.txt') as f:
@@ -46,27 +58,23 @@ if sc.rtm_connect():
                 add_sound_match = add_sound_regex.match(event['text'])
 
                 if play_match:
-                    print whitelist[event['user']] + ' plays ' + play_match.group(1)
+                    message = whitelist[event['user']] + ' plays ' + play_match.group(1)
                     command = player + ' ' + sounds_dir + '/' + play_match.group(1) + '.' + filetype
-                    if debug: print 'Running command: ' + command
-                    os.system(command)
+                    action(command, message)
                 elif speak_match:
-                    print whitelist[event['user']] + ' speaks ' + speak_match.group(1)
-                    command = text2voice + ' -110 "' + speak_match.group(1) + '"'
-                    if debug: print 'Running command: ' + command
-                    os.system(command)
+                    message = whitelist[event['user']] + ' speaks ' + speak_match.group(1)
+                    command = text2voice + ' -a 20 -s 110 "' + speak_match.group(1) + '"'
+                    action(command, message)
                 elif play_yt_match:
-                    print whitelist[event['user']] + ' plays youtube video ' + play_yt_match.group(1)
+                    message = whitelist[event['user']] + ' plays youtube video ' + play_yt_match.group(1)
                     command = './yt-audio.sh ' + play_yt_match.group(1)
                     if play_yt_match.group(2): command += play_yt_match.group(2)
-                    if debug: print 'Running command: ' + command
-                    os.system(command)
+                    action(command, message)
                 elif add_sound_match:
-                    print whitelist[event['user']] + ' adds sound ' + add_sound_match.group(1) + ' from youtube video ' + add_sound_match.group(2)
+                    message = whitelist[event['user']] + ' adds sound ' + add_sound_match.group(1) + ' from youtube video ' + add_sound_match.group(2)
                     command = './yt-add-sound.sh ' + add_sound_match.group(1) + ' ' + add_sound_match.group(2)
                     if add_sound_match.group(3): command += add_sound_match.group(3)
-                    if debug: print 'Running command: ' + command
-                    os.system(command)
+                    action(command, message)
         time.sleep(1);
 else:
     print 'Connection failed, invalid token?'
