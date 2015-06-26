862f0f3c9bb2567bd47e88ed7566d631b36f3002
Casey Fulton <casey.fulton@adelaide.edu.au>
2015-06-26 12:00:00 +0930

    Overwrite when doing an add-sound.

--- yt-add-sound.sh
+++ yt-add-sound.sh
@@ -3,7 +3,7 @@ file=$(tempfile).m4a
 youtube-dl -f 140 ${2} -o $file
 if [ $# -eq 1 ]
 then
-    avconv -i $file sounds/${1}.mp3
+    avconv -y -i $file sounds/${1}.mp3
 else
-    avconv -i $file -ss ${3} -t ${4} sounds/${1}.mp3
+    avconv -y -i $file -ss ${3} -t ${4} sounds/${1}.mp3
 fi
