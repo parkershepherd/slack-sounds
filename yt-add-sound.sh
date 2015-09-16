28d9a1bacd346a67a01aa5ac9c508c7e82abe2f8
Casey <casey@caseyfulton.com>
2015-09-16 13:01:28 +0930

    Fixed yt-add-sound script to work from anywhere.

--- yt-add-sound.sh
+++ yt-add-sound.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-file=$(tempfile).m4a
-youtube-dl -f 140 ${2} -o $file
+FILE=$(tempfile).m4a
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+youtube-dl -f 140 ${2} -o $FILE
 if [ $# -le 2 ]
 then
-    avconv -y -i $file sounds/${1}.mp3
+    avconv -y -i $FILE $DIR/sounds/${1}.mp3
 else
-    avconv -y -i $file -ss ${3} -t ${4} sounds/${1}.mp3
+    avconv -y -i $FILE -ss ${3} -t ${4} $DIR/sounds/${1}.mp3
 fi
